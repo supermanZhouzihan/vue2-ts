59986ba40e4c5ba100a27618c1d1d6850291e609
zhouzihan <350636553@qq.com>
2024-08-03 16:09:26 +0800

    测试推送远程仓库

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld :msg="test" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  data() {
+    return {
+      test: "",
+    };
+  }
+
+  mounted() {
+    console.log("组件已挂载");
+    this.init(2,'test');
+  }
+
+  private init(num:number,msg:string): void {
+    console.log("执行init方法");
+    let a: number =12;
+    a = 34;
+
+
+    let com ;
+    com=12
+
+    console.log(com)
+
+  }
+}
 </script>
